ff1a17f601844b4d8f3ed49697b3830796238291
carolinabtt <carolinabattaglia@hotmail.com>
2025-06-16 17:25:53 +0200

    Adding sources

--- models/_staging/stg_orders.sql
+++ models/_staging/stg_orders.sql
@@ -1,16 +1,13 @@
 {{ config(
-    materialized='table',
+    materialized='incremental',
+    incremental_strategy='insert_overwirte',
     partition_by = {'field' : 'order_date', 'data_type' : 'date'}
     )
 }}
 
 with source as (
 
-    {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
-    #}
-    select * from {{ ref('raw_orders') }}
+    select * from {{ source('raw_dataset','raw_orders') }}
 
 ),
 
@@ -21,7 +18,7 @@ renamed as (
         user_id as customer_id,
         order_date,
         cast(concat(substr(cast(order_date as string),1,4), substr(cast(order_date as string),6,2), 
-            substr(cast(order_date as string),9,2)) as int64) as pk_anyomesdia,
+            substr(cast(order_date as string),9,2)) as int64) as order_yyyymmdd,
         status,
         current_timestamp() as updated_at
 
